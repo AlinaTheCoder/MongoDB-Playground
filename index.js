f5beb7a4d1ae77de9fdf450e8b3da5103142d5de
AlinaTheCoder <alinalearns6@gmail.com>
2025-09-11 12:58:36 +0500

    northflank server listening issue resolve try

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
